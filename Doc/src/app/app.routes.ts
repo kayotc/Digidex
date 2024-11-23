14b9f9e933819c7518550495e4c9439e7521c2e5
Kayo Vinicius <151939515+kayotc@users.noreply.github.com>
2024-11-23 17:04:41 -0300

    Delete Doc directory

--- Doc/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-export const routes: Routes = [
-  { path: '', redirectTo: 'app', pathMatch: 'full' },
-  { path: 'home', component: HomeComponent },
-  { path: 'footer', component: FooterComponent }
-];
