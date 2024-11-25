c2f9f1c1555baaef9aa9242964fbe348ad39d403
Kayo Vinicius <151939515+kayotc@users.noreply.github.com>
2024-11-25 09:41:23 -0300

    Add files via upload

---
+++ src/app/components/details/details.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { SendDetailsService } from '../../services/send-details.service';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-details',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.scss'
+})
+export class DetailsComponent {
+
+  digimonDetails: any;
+
+  constructor(private rotas: Router, private details: SendDetailsService) {
+
+    this.digimonDetails= this.details.getDigimonDetails();
+  }
+
+  goToChoice(){
+    this.rotas.navigate(["choice"]);
+  }
+
+  goToTeam(){
+    this.rotas.navigate(["team"]);
+  }
+
+
+
+}
